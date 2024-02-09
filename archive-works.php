5e62b467d5c0887e1752f4d25bcd7869e76c6fb3
hituji_code <k.m.hituji3340@gmail.com>
2024-02-09 12:35:48 +0900

    gulp動かない

--- archive-works.php
+++ archive-works.php
@@ -11,27 +11,27 @@
           <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
               <li class="p-cards__item">
                 <a href="<?php the_permalink(); ?>" class="p-card">
-                      <div class="p-card__img">
-                      <?php
-                      // サムネイルがあるかどうかをチェック
-                      if (has_post_thumbnail()) :
-                        // サムネイルの HTML を直接出力
-                        echo get_the_post_thumbnail(null, 'full');
-                      else :
-                        // CFSで指定された画像があるかどうかをチェック
-                        $works_gallery = CFS()->get('works_gallery');
-                        $cfs_image_url = !empty($works_gallery) ? $works_gallery[0]['works_img'] : '';
+                  <div class="p-card__img">
+                    <?php
+                    // サムネイルがあるかどうかをチェック
+                    if (has_post_thumbnail()) :
+                      // サムネイルの HTML を直接出力
+                      echo get_the_post_thumbnail(null, 'full');
+                    else :
+                      // CFSで指定された画像があるかどうかをチェック
+                      $works_gallery = CFS()->get('works_gallery');
+                      $cfs_image_url = !empty($works_gallery) ? $works_gallery[0]['works_img'] : '';
 
-                        // CFSで指定された画像がある場合、その画像を直接表示
-                        if (!empty($cfs_image_url)) :
-                          echo '<img src="' . esc_url($cfs_image_url) . '" alt="' . esc_attr(get_the_title()) . '">';
-                        else :
-                          // サムネイルもCFSの画像もない場合、'No Image' 画像を表示
-                          echo '<img src="' . esc_url(get_theme_file_uri('/assets/images/common/noimage@2x.webp')) . '" alt="No Image">';
-                        endif;
+                      // CFSで指定された画像がある場合、その画像を直接表示
+                      if (!empty($cfs_image_url)) :
+                        echo '<img src="' . esc_url($cfs_image_url) . '" alt="' . esc_attr(get_the_title()) . '">';
+                      else :
+                        // サムネイルもCFSの画像もない場合、'No Image' 画像を表示
+                        echo '<img src="' . esc_url(get_theme_file_uri('/assets/images/common/noimage@2x.webp')) . '" alt="No Image">';
                       endif;
-                      ?>
-                    </div>
+                    endif;
+                    ?>
+                  </div>
                   <div class="p-card__category">
                     <?php $terms = get_the_terms(get_the_ID(), 'genre'); ?>
                     <?php if ($terms && !is_wp_error($terms)) : ?>
